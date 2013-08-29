9d3f8838302b4a9d87ada702759594dd6674288a
huayuxi <semstouch@gmail.com>
2013-08-29 11:19:19 +0800

    新闻Dao测试类

---
+++ test/com/dao/impl/NewsDaoImplTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright:Copyright(c)2013-2014
+ * Company:厦门市智联信通物联网科技有限公司
+ * @version 1.0
+ */
+package com.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewsDaoImplTest {
+	
+	@Test
+	public void testAddNews() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDelNews() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testUpdateNews() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testQueryNews() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCountNews() {
+		fail("Not yet implemented");
+	}
+	
+}
