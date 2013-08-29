5cfa83c534279e8cc50b7c62a09e6cba171f2d70
huayuxi <semstouch@gmail.com>
2013-08-29 11:11:08 +0800

    新增测试类

---
+++ test/com/util/StringUtilTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright:Copyright(c)2013-2014
+ * Company:厦门市智联信通物联网科技有限公司
+ * @version 1.0
+ */
+package com.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+	
+	@Test
+	public void testFindTo2CharSubString() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFindTo2Char() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testReplaceTo2Char() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEndSubString() {
+		fail("Not yet implemented");
+	}
+	
+}
